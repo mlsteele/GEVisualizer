39763a00326bc91c3ac501ab3327c78b1a323f49
Miles Steele <miles@milessteele.com>
2013-04-26 17:55:39 -0400

    factor out ofxUIColorSet.h

--- src/LayoutApp/LayoutApp.h
+++ src/LayoutApp/LayoutApp.h
@@ -13,30 +13,9 @@
 #include "LayoutRenderer.h"
 #include "SkeletonRenderer.h"
 #include "LocationData.h"
+#include "ofxUIColorSet.h"
 // #include "GELink.h"
 #include "GEVisualizer.h"
-
-
-// TODO this struct belongs elsewhere
-typedef struct {
-    ofColor color_back = OFX_UI_COLOR_BACK;
-    ofColor color_outline = OFX_UI_COLOR_OUTLINE;
-    ofColor color_outline_highlight = OFX_UI_COLOR_OUTLINE_HIGHLIGHT;
-    ofColor color_fill = OFX_UI_COLOR_FILL;
-    ofColor color_fill_highlight = OFX_UI_COLOR_FILL_HIGHLIGHT;
-    ofColor color_padded_rect = OFX_UI_COLOR_PADDED;
-    ofColor color_padded_rect_outline = OFX_UI_COLOR_PADDED_OUTLINE;     
-
-    void apply(ofxUICanvas& c) const {
-        c.setColorBack(color_back);
-        c.setColorOutline(color_outline);
-        c.setColorOutlineHighlight(color_outline_highlight);
-        c.setColorFill(color_fill);
-        c.setColorFillHighlight(color_fill_highlight);
-        c.setColorPadded(color_padded_rect);
-        c.setColorPaddedOutline(color_padded_rect_outline);
-    }
-} ofxUIColorSet;
 
 
 class LayoutApp : public ViewApp {
